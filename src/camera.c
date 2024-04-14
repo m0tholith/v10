9877ca22187c272aecfab6eb4ea9294c3fbc0b44
m0tholith <48518572+Motholith@users.noreply.github.com>
2024-04-14 16:19:35 +0300

    Added euler angle to quaternion conversion

--- src/camera.c
+++ src/camera.c
@@ -1,6 +1,8 @@
 #include "camera.h"
 
+#define X_AXIS ((vec3s){{1.0f, 0.0f, 0.0f}})
 #define Y_AXIS ((vec3s){{0.0f, 1.0f, 0.0f}})
+#define Z_AXIS ((vec3s){{0.0f, 0.0f, 1.0f}})
 
 Camera cameraCreate(vec3s position, versors quaternion) {
     Camera camera = (Camera){
@@ -20,7 +22,10 @@ void cameraLookAt(Camera *camera, vec3s target) {
     camera->Quaternion = glms_quat_forp(camera->Position, target, Y_AXIS);
 }
 void cameraSetEulerAngles(Camera *camera, vec3s eulerAngles) {
-
+    versors quatX = glms_quatv(eulerAngles.x, X_AXIS);
+    versors quatY = glms_quatv(eulerAngles.y, Y_AXIS);
+    versors quatZ = glms_quatv(eulerAngles.z, Z_AXIS);
+    camera->Quaternion = glms_quat_mul(glms_quat_mul(quatY, quatX), quatZ);
 }
 void cameraSetQuaternion(Camera *camera, versors quaternion) {
     camera->Quaternion = quaternion;
