acd8404a69a2441bd483621d361c3824eea70ba1
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-06-22 19:07:11 +0300

    Added hashmap for more efficient texture creation

--- src/texture.c
+++ src/texture.c
@@ -3,10 +3,24 @@
 #include "glad/glad.h"
 #include "stb/stb_image.h"
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 #include <string.h>
+
+#define HASHMAP_SIZE 0xFFFF
+unsigned int texHashmap[HASHMAP_SIZE] = {0};
+
+unsigned int crc32b(unsigned char *message);
+void texHashmapInsert(unsigned char *textureFile, unsigned int texture);
+unsigned int texHashmapGet(unsigned char *textureFile, bool *success);
 
 unsigned int textureCreate(const char *textureFile, enum TEXTURETYPE type) {
     unsigned int texture;
+
+    bool success;
+    texture = texHashmapGet(textureFile, &success);
+    if (success) {
+        return texture;
+    }
 
     int width, height, numColorChannels;
     unsigned char *data;
@@ -34,5 +48,47 @@ unsigned int textureCreate(const char *textureFile, enum TEXTURETYPE type) {
     glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(data);
 
+    texHashmapInsert(textureFile, texture);
     return texture;
+}
+
+void texHashmapInsert(unsigned char *textureFile, unsigned int texture) {
+    unsigned int hash = crc32b(textureFile);
+    texHashmap[hash] = texture;
+}
+unsigned int texHashmapGet(unsigned char *textureFile, bool *success) {
+    unsigned int hash = crc32b(textureFile);
+    unsigned int entry = texHashmap[hash];
+    // if the entry in hashmap is already set
+    // opengl texture ids (afaik) start at 1
+    if (texHashmap[hash] == 0) {
+#define REDCODE "\033[0;31m"
+#define ENDCODE "\033[0m"
+        fprintf(stderr, REDCODE "no entry found for texture \"%s\"\n" ENDCODE,
+                textureFile);
+        *success = false;
+        return 0;
+    }
+    *success = true;
+    printf("entry found for texture \"%s\"\n", textureFile);
+    return entry;
+}
+
+// https://stackoverflow.com/a/21001712/24750185
+unsigned int crc32b(unsigned char *message) {
+    int i, j;
+    unsigned int byte, crc, mask;
+
+    i = 0;
+    crc = 0xFFFFFFFF;
+    while (message[i] != 0) {
+        byte = message[i]; // Get next byte.
+        crc = crc ^ byte;
+        for (j = 7; j >= 0; j--) { // Do eight times.
+            mask = -(crc & 1);
+            crc = (crc >> 1) ^ (0xEDB88320 & mask);
+        }
+        i = i + 1;
+    }
+    return (~crc) % 0xFFFF;
 }
