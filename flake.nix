da13e55e99600ec3d4aee1ebca65592e7d96526e
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-07-24 10:14:22 +0300

    Moved to using flake for shell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Nix shell for packages required to build this project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cmake
+          glfw
+          cglm
+          assimp
+
+          # debugging
+          (pkgs.callPackage ./ugdb.nix { })
+        ];
+      };
+    };
+}
