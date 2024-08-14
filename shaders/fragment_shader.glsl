b7d2bb4d36a5d3ea7dbe79031f78f8b2ab4102e8
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-08-14 14:53:53 +0300

    Moved to opengl 4.6

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -1,4 +1,4 @@
-#version 330 core
+#version 460 core
 
 in vec3 vColor;
 in vec2 vTexCoord;
--- src/window.c
+++ src/window.c
@@ -9,8 +9,8 @@ void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
 GLFWwindow *windowCreate() {
     glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
