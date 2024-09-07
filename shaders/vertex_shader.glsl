d5b2dd498f9cecc7039c1ff88c736175c4af062c
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-09-07 20:14:13 +0300

    Added psx warping effect

--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -11,11 +11,18 @@ out vec3 vNormal;
 
 uniform mat4 projectionFromModel;
 
-void main()
-{
+const float strength = 20;
+vec4 vertex_warp(vec4 pos) {
+    pos.xy = (pos.xy + vec2(1.0)) * vec2(320.0 / strength, 240.0 / strength) * 0.5;
+    pos.xy = round(pos.xy);
+    pos.xy = pos.xy * 2 / vec2(320.0 / strength, 240.0 / strength) - vec2(1.0, 1.0);
+    return pos;
+}
+
+void main() {
     vColor = vertColor;
     vTexCoord = vertTexCoord;
     vNormal = vertNormal;
 
-    gl_Position = projectionFromModel * vec4(vertPos, 1.0f);
+    gl_Position = vertex_warp(projectionFromModel * vec4(vertPos, 1.0f));
 }
