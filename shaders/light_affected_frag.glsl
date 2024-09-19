cc5295728fd078a8a7b4b738e6720a8b19687407
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-09-19 21:24:06 +0300

    Added specular lighting (blinn-phong) to default shader

--- shaders/light_affected_frag.glsl
+++ shaders/light_affected_frag.glsl
@@ -7,10 +7,14 @@ in vec3 vPos;
 
 out vec4 FragColor;
 
+uniform vec3 _cameraWorldPosition;
+
 uniform vec3 light_position;
 uniform vec3 light_color;
 
 const vec3 ambient_color = vec3(0.1f);
+const float specular_strength = 0.15f;
+const int shininess = 6;
 
 void main()
 {
@@ -18,5 +22,11 @@ void main()
     float diffuseValue = max(dot((vNormal), lightDir), 0.0f);
     vec3 diffuse = diffuseValue * light_color;
 
-    FragColor = vec4((diffuse + ambient_color) * vColor, 1.0f);
+    vec3 viewDir = normalize(_cameraWorldPosition - vPos);
+    vec3 reflectDir = reflect(-lightDir, vNormal);
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float spec = pow(max(dot(vNormal, halfwayDir), 0.0f), pow(2, shininess));
+    vec3 specular = specular_strength * spec * light_color;
+
+    FragColor = vec4((diffuse + ambient_color + specular) * vColor, 1.0f);
 }
