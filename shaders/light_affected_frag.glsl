c4f2750b37ce3a158f6c80ad0e4c37bc83059e7e
m0tholith <48518572+m0tholith@users.noreply.github.com>
2024-09-26 13:38:54 +0300

    Separated necessary light calculation functions

--- shaders/light_affected_frag.glsl
+++ shaders/light_affected_frag.glsl
@@ -30,29 +30,45 @@ struct Light {
 };
 uniform Light light;
 
-float attenuation(Light light) {
-    float distanceRatioSqr = pow(min(distance(light.position, vPos) / light.distance, 1), 2);
-    float att = light.intensity * pow(1 - distanceRatioSqr, 2) / (1 + light.decay * distanceRatioSqr);
+float attenuation(vec3 lightPos, float lightDistance, float lightIntensity, float lightDecay);
+vec3 diffuse(vec3 lightPos, vec3 lightDiffuse);
+vec3 specular(vec3 lightPos, vec3 lightSpecular);
+vec3 calc_point_light(Light light);
+
+void main()
+{
+    FragColor = vec4(calc_point_light(light), 1.0f);
+}
+
+float attenuation(vec3 lightPos, float lightDistance, float lightIntensity, float lightDecay)
+{
+    float distanceRatioSqr = pow(min(distance(lightPos, vPos) / lightDistance, 1), 2);
+    float att = lightIntensity * pow(1 - distanceRatioSqr, 2) / (1 + lightDecay * distanceRatioSqr);
     return att;
+}
+vec3 diffuse(vec3 lightPos, vec3 lightDiffuse)
+{
+    vec3 lightDir = normalize(lightPos - vPos);
+    float diffuseValue = max(dot((vNormal), lightDir), 0.0f);
+    vec3 diffuse = lightDiffuse * diffuseValue * _material.diffuse * vColor;
+    return diffuse;
+}
+vec3 specular(vec3 lightPos, vec3 lightSpecular)
+{
+    vec3 lightDir = normalize(lightPos - vPos);
+    vec3 viewDir = normalize(_cameraWorldPosition - vPos);
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float spec = pow(max(dot(vNormal, halfwayDir), 0.0f), pow(2, _material.shininess));
+    vec3 specular = lightSpecular * spec * _material.specular * _material.specular_strength;
+    return specular;
 }
 vec3 calc_point_light(Light light)
 {
     vec3 ambient = light.ambient * _material.ambient;
-
-    vec3 lightDir = normalize(light.position - vPos);
-    float diffuseValue = max(dot((vNormal), lightDir), 0.0f);
-    vec3 diffuse = light.diffuse * diffuseValue * _material.diffuse * vColor;
-
-    vec3 viewDir = normalize(_cameraWorldPosition - vPos);
-    vec3 halfwayDir = normalize(lightDir + viewDir);
-    float spec = pow(max(dot(vNormal, halfwayDir), 0.0f), pow(2, _material.shininess));
-    vec3 specular = light.specular * spec * _material.specular * _material.specular_strength;
+    vec3 diffuse = diffuse(light.position, light.diffuse);
+    vec3 specular = specular(light.position, light.specular);
 
     vec3 resultColor = diffuse + ambient + specular;
 
-    return resultColor * attenuation(light);
-}
-void main()
-{
-    FragColor = vec4(calc_point_light(light), 1.0f);
+    return resultColor * attenuation(light.position, light.distance, light.intensity, light.decay);
 }
